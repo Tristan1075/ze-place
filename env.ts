943ea7b5f7fa7eef4981c803bcc096e258de6b11
alpheonix <arthurblanc98@gmail.com>
2021-06-22 14:53:57 +0200

    change s3 key

--- env.ts
+++ env.ts
@@ -12,7 +12,7 @@ export const MAPBOX_TOKEN =
 export const PUBLIC_KEY_STRIPE =
   'pk_test_51IvjYaIeDqziwrFRY8xIuqAbYSJWryVnPyBZYptrXu3VvzHN7zq2Jh4knobnAAk5EmfVIkgj8iuHOp86BSHpWtXQ004oWdXNLJ';
 
-export const REACT_APP_ACCESS_ID = 'AKIA6J5RFEGPGJY33TFQ';
+export const REACT_APP_ACCESS_ID = 'AKIA6J5RFEGPC6LYVNO5';
 export const REACT_APP_ACCESS_KEY = 'R90Y/9EqJsgJTrJzLEL0KeroeIFZdMHy+s7tgSq7';
 export const REACT_APP_BUCKET_NAME = 'ze-place';
 export const REACT_APP_REGION = 'eu-west-3';
